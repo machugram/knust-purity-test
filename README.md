ae508ec03c7735c7acfc10619c67e97159518ddb
Rexford Machu <rexfordmachu@gmail.com>
2022-11-23 00:25:21 +0000

    Improving ML Magic.

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # knust-purity-test
  Some Sort Of Bucket List for stuff you have done in KNUST      
- 
+
 https://knustwtf.netlify.app
--- app-name/src/ExecutorFunctions.tsx
+++ app-name/src/ExecutorFunctions.tsx
@@ -8,6 +8,7 @@ export function PerformMLMagic(TodoItemData: Map<string, TodoItem>){
         MLData.push(dataItem.text)
         totalScore+=dataItem.score
       }
+      totalScore = totalScore <=100 ? totalScore : 100
     });
     let jsonMessage = {
       "message": displayMessage(MLData,totalScore),
@@ -31,13 +32,35 @@ export function PerformMLMagic(TodoItemData: Map<string, TodoItem>){
   }
 
   function displayMessage(describeData: Array<string>, score:number){
+    let randomResponses = ["You are a discovery","Someone actually thinks you are learning hard","Your parents are proud. ",
+    "And daswhy you are the GOAT. \n Sike!"
+  ]
+
+  let tierOneResponses = ["Get a life. Jeez", "This should be a bucket list for you.","Please ask your friends, I am just a mirror."]
     var tierOne = "Has relations with a TA"
-    var tierTwo = ""
+    var tierTwo = "Has never submitted an original assignment "
+    var tierThree = "Blocked your class rep"
     if (score < 10){
-      return "Get a life. Jeez"
+      return tierOneResponses[Math.floor(Math.random() * tierOneResponses.length)]
     }
+
     if (describeData.includes(tierOne)){
-      return "I hope you at least gained some marks you hoe"
+      randomResponses.push("I hope you at least gained some marks you erohw")
+      return randomResponses[Math.floor(Math.random() * randomResponses.length)]
     }
-      return "Your parents are proud. "
+    if (describeData.includes(tierTwo)){
+      randomResponses.push("Oh? Oh? How? Why? Not one assignment?")
+      return randomResponses[Math.floor(Math.random() * randomResponses.length)]
+
+    } 
+    if (describeData.includes(tierThree)){
+      randomResponses.push("Do you attempt on going to class?")
+      return randomResponses[Math.floor(Math.random() * randomResponses.length)]
+
+    } 
+    else if(score>85){
+      return "Legend. And that's on period. Purrrr💅🏾"
+    }
+
+    return randomResponses[Math.floor(Math.random() * randomResponses.length)]
   }
